97b807722c439cfdc5e95b68cac89174b896876f
Aleksey <pq1121@mail.ru>
2025-04-03 21:27:16 +0300

    feat add new function List

--- hw_3/tasks.py
+++ hw_3/tasks.py
@@ -1,6 +1,7 @@
 def malloc(size):
     return [None] * size
 
+
 class List:
 
     def __init__(self, size=1):
@@ -8,8 +9,10 @@ class List:
         self.__memory = [None] * size
         self.__count = 0
 
+
     def check_index(self, index):
         if index > self.__count or index < 0: raise ValueError("Индекс указан не верно")
+
 
     def new_size(self):
         self.__size = self.__size + (self.__size // 2 + 1)
@@ -19,25 +22,81 @@ class List:
             new_memory[i] = self.__memory[i]
         self.__memory = new_memory
 
-    def add(self,value):
+
+    def add(self, value):
 
         if self.__size == self.__count:
             self.new_size()
         self.__memory[self.__count] = value
         self.__count += 1
 
-    def insert(self,index,value):
+
+    def insert(self, index, value):
         self.check_index(index)
 
         if self.__size == self.__count:
             self.new_size()
         i = self.__count
 
-        while i >= index and i > index:
+        while i > index:
             self.__memory[i] = self.__memory[i-1]
             i -= 1
         self.__memory[index] = value
         self.__count += 1
 
+
+    def remove(self, value):
+        target_index = -1
+
+        if self.__size == self.__count:
+            self.new_size()
+
+        for i in range(self.__count):
+
+            if self.__memory[i] == value:
+                target_index = i
+                break
+
+        if target_index == -1: raise ValueError("Элемент не найдет")
+
+        for i in range(target_index,self.__count):
+            self.__memory[i] = self.__memory[i+1]
+        self.__count -= 1
+
+
+    def count(self, value):
+        count = 0
+
+        for i in range(self.__count):
+
+            if self.__memory[i] == value:
+                count += 1
+        return count
+
+
+    def find(self, value):
+
+        for i in range(self.__count):
+
+            if self.__memory[i] == value:
+                return i
+
+
+    def pop(self, index):
+        self.check_index(index)
+        value_index = self.__memory[index]
+
+        if self.__size == self.__count:
+            self.new_size()
+
+        for i in range(index,self.__count):
+            self.__memory[i] = self.__memory[i+1]
+        self.__count -= 1
+        return value_index
+
+
+    def add_front(self, value):
+        self.insert(0, value)
+
     def __str__(self):
-        return f"{self.__memory[:self.__count]}"
+        return f"{self.__memory}\nколичество: {self.__count}"
